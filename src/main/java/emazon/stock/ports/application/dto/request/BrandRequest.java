3e08bede642cff098b8e518661731af328c282c6
Daniel Ureche <bryan.danielureche@gmail.com>
2024-08-25 15:21:55 -0500

    i developed the validations for the dto request.

--- src/main/java/emazon/stock/ports/application/dto/request/BrandRequest.java
+++ src/main/java/emazon/stock/ports/application/dto/request/BrandRequest.java
@@ -1,5 +1,8 @@
 package emazon.stock.ports.application.dto.request;
 
+import emazon.stock.configuration.ExceptionConstants;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +11,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BrandRequest {
-    private Long id;
+
+    @NotBlank(message = ExceptionConstants.BRAND_NAME_REQUIRED)
+    @Size(max = 50, message = ExceptionConstants.BRAND_NAME_LENGTH)
     private String name;
+
+    @NotBlank(message = ExceptionConstants.BRAND_DESCRIPTION_REQUIRED)
+    @Size(max = 120, message = ExceptionConstants.BRAND_DESCRIPTION_LENGTH)
     private String description;
 }
